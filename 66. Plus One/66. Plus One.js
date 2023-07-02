67c7bf71606bc616d076c6de069078085b6c03ee
mohamedelsayed15 <mo.elsayed621654@gmail.com>
2023-07-02 06:09:38 +0300

    66. Plus One

---
+++ 66. Plus One/66. Plus One.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let number = digits.toString()//convert to string
+
+    number = number.replace(/,/g, '') //removing ',' from the string
+    number = BigInt(number)// convert to BigInt type
+    number = number+ BigInt(1) // add BigInt to BigInt only
+    number = number.toString() // convert back to string so we can loop through it
+
+    const array = []
+
+    for (let i = 0; i < number.length; i++){
+        array.push((+number[i]))// pushing the number to the array
+    }
+
+    return array
+};
